65527b2d1c27ef80aa1236b7db161f589aa9e353
mararaed <edosamarara@gmail.com>
2021-04-24 03:37:48 -0800

    cloud database

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ process.on("uncaughtException", (err) => {
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
